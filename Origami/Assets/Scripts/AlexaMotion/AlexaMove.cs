13fb75af4dae26f73b7186dd71cf5b33f968054f
shiweiwei0114 <wshi@uw.edu>
2016-05-22 02:19:10 -0700

    Added Alexa motion file(unfinished version)

---
+++ Origami/Assets/Scripts/AlexaMotion/AlexaMove.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlexaMove : MonoBehaviour {
+
+    //Declare variables
+    Vector3 originalPosition;
+    Quaternion originalRotation;
+
+    bool move = false;
+    float resetTime = 0;
+
+	// Use this for initialization
+	void Start () {
+        //Grab the initial postion of the cylider when the app starts.
+        originalPosition = this.transform.localPosition;
+        originalRotation = this.transform.localRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (move)
+            resetTime++;
+
+        if (resetTime >= 100)
+        {
+            move = false;
+            //Destroy(this.GetComponent<Rigidbody>());
+            //this.transform.position = originalPosition;
+            OnReset();
+            resetTime = 0;
+        }
+    }
+
+    //Called by GazeGestureManager when the user performs a select gesture.
+    void OnSelect()
+    {
+        if (!this.GetComponent<Rigidbody>())
+        {
+            originalPosition = this.transform.localPosition;
+            var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+            rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+            rigidbody.AddForce(Camera.main.transform.forward * 500);
+            move = true;
+        }
+    }
+
+    void OnReset()
+    {
+        // If the sphere has a Rigidbody component, remove it to disable physics.
+        var rigidbody = this.GetComponent<Rigidbody>();
+        if (rigidbody != null)
+        {
+            DestroyImmediate(rigidbody);
+        }
+
+        // Put the sphere back into its original local position.
+        this.transform.localPosition = originalPosition;
+        this.transform.localRotation = originalRotation;
+    }
+
+    // Called by SpeechManager when the user says the "Drop sphere" command
+    void OnDrop()
+    {
+        // Just do the same logic as a Select gesture.
+        OnSelect();
+    }
+}
---
+++ Origami/Assets/Scripts/AlexaMotion/TapDragToPlaceAlexa.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+using System.Collections;
+
+public class TapDragToPlaceAlexa : MonoBehaviour {
+
+    public GameObject shootingStand;
+    bool placing = true;
+    GestureRecognizer recognizer;
+
+	// Use this for initialization
+	void Start () {
+        SpatialMapping.Instance.DrawVisualMeshes = true;
+        recognizer.SetRecognizableGestures(GestureSettings.Hold);
+        recognizer.HoldStartedEvent += Recognizer_HoldStartedEvent;
+        recognizer.HoldCompletedEvent += Recognizer_HoldCompletedEvent;
+        recognizer.HoldCanceledEvent += Recognizer_HoldCompletedEvent;
+
+    }
+
+    private void Recognizer_HoldCompletedEvent(InteractionSourceKind source, Ray headRay)
+    {
+        Debug.LogWarning("HoldCompletedEvent");
+        //throw new System.NotImplementedException();
+    }
+
+    private void Recognizer_HoldStartedEvent(InteractionSourceKind source, Ray headRay)
+    {
+        Debug.LogWarning("Hold Start");
+        throw new System.NotImplementedException();
+    }
+
+    //Called by GazeGestureManager when the user performs a Select gesture
+    //but how about drag?
+    void OnSelect()
+    {
+        //On each Select gesture, toggle whether the use is in placing mode.
+        
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+	    //If the user is in placing mode, update the placement to match the user's gaze
+        if (placing)
+        {
+            //Do a raycasr into the world that will only hit the Spatial Mapping Mesh
+            var headPosition = Camera.main.transform.position;
+            var gazeDirection = Camera.main.transform.forward;
+
+            RaycastHit hitInfo;
+            if (Physics.Raycast(headPosition, gazeDirection, out hitInfo, 30.0f, SpatialMapping.PhysicsRaycastMask))
+            {
+                //Move this object's parent object to where the raycast hit the Spatial Mapping mesh.
+                this.transform.parent.position = hitInfo.point;
+
+                //Rotate this object's parent object to face the user
+                Quaternion toQuat = Camera.main.transform.localRotation;
+                toQuat.x = 0;
+                toQuat.y = 0;
+                this.transform.parent.rotation = toQuat;
+            }
+        }
+	}
+}
